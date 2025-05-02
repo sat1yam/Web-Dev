cdf34d8d49ded9da118a1054178a13699c50746b
sat1yam <satyam.mishra6@wipro.com>
2025-05-02 17:12:18 +0530

    Update on java version 1

--- EvenOdd.java
+++ EvenOdd.java
@@ -6,7 +6,6 @@ public class EvenOddChecker {
 
     public static void main(String[] args) {
         System.out.println("Even or Odd numbers from 1 to 20:");
-
         for (int i = 1; i <= 20; i++) {
             if (isEven(i)) {
                 System.out.println(i + " is even");
