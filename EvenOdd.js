a4c8ce2d89e294050937bc77ec6b4652c5838c9e
sat1yam <satyam.mishra6@wipro.com>
2025-05-02 17:15:03 +0530

    Update on JavaScript on version 2

--- EvenOdd.js
+++ EvenOdd.js
@@ -1,9 +1,7 @@
-
 function isEven(num) {
     return num % 2 === 0;
 }
 
-// Loop through numbers 1 to 10 and log results
 for (let i = 1; i <= 10; i++) {
     if (isEven(i)) {
         console.log(i + " is even");
